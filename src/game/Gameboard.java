517a24576c83a23bd03698182adbbe4356e441df
spirotris <38663830+spirotris@users.noreply.github.com>
2019-03-14 19:57:57 +0100

    Gameboard 2d array implemented
    Flexible gameboard size with parameters for width+height, method for getting current status of a specific tile with 'getPoint(int x, int y)' method.

--- src/game/Gameboard.java
+++ src/game/Gameboard.java
@@ -1,6 +1,23 @@
 package game;
 
 public class Gameboard {
-    public Gameboard() {
+    
+    private final int HEIGHT;
+    private final int WIDTH;
+    private final int[][] boardGrid;
+    
+    public Gameboard(int width, int height) {
+        this.HEIGHT = height;
+        this.WIDTH = width;
+        boardGrid = new int[height][width];
+         for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                boardGrid[i][j] = 0;
+            }
+        }
+    }
+    
+    public int getPoint(int x, int y) {
+        return boardGrid[y][x];
     }
 }
