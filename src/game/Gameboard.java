41806abc093b0fd14bb6745e9e41289c18922e02
Ohrling <ohrling@users.noreply.github.com>
2019-03-15 17:11:18 +0100

    A little bit of refactoring

--- src/game/Gameboard.java
+++ src/game/Gameboard.java
@@ -16,17 +16,21 @@ public class Gameboard {
         boardGrid = new Point[HEIGHT][WIDTH];
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
-                if(i == 0 || j == 0 || i == HEIGHT -1 || j == WIDTH -1)
-                    boardGrid[i][j] = new Point(i,j,WALL);
-                else if(i == 10 && j == 1) {
-                    boardGrid[i][j] = new Point(i,j,CHARACTER);
-                }
-                else
-                    boardGrid[i][j] = new Point(i,j,FLOOR);
+                addPointToBoard(i, j);
             }
         }
     }
-    
+
+    private void addPointToBoard(int i, int j) {
+        if(i == 0 || j == 0 || i == HEIGHT -1 || j == WIDTH -1)
+            boardGrid[i][j] = new Point(i,j,WALL);
+        else if(i == (HEIGHT / 2) && j == 1) {
+            boardGrid[i][j] = new Point(i,j,CHARACTER);
+        }
+        else
+            boardGrid[i][j] = new Point(i,j,FLOOR);
+    }
+
     public Point getPoint(int x, int y) {
         return boardGrid[y][x];
     }
