aea21c1e812a86b672323b24727ad979cb37851f
Ohrling <ohrling@users.noreply.github.com>
2019-03-15 13:25:22 +0100

    Refactored the point test, now checking diff parameters

--- tests/PointTest.java
+++ tests/PointTest.java
@@ -1,20 +1,33 @@
 import game.Point;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(JUnitParamsRunner.class)
 public class PointTest {
+
     @Test
-    public void createPointGetTypeByInt() {
+    @Parameters({
+            "1,1,0", // Floor
+            "0,0,1", // Wall
+            "2,2,2", // Character
+            "5,5,3", // Treasure
+            "20,10,4", // Door
+            "10,5,5" // Laser
+    })
+    public void createPointGetTypeByInt(int y, int x, int status) {
         // Assert
-        Point p = new Point(0,0);
-        p.setStatus(0);
+        Point p = new Point(y,x);
+        p.setStatus(status);
 
         // Act
         int actual = p.getStatus();
 
         // Assert
-        assertEquals(0,actual);
+        assertEquals(status,actual);
     }
 }
 
