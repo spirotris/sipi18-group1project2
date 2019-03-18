4f7943409dafc348cb48cab2ad0308795320ce98
spirotris <38663830+spirotris@users.noreply.github.com>
2019-03-18 19:20:22 +0100

    Added test to check if keypress moves character

--- tests/GuiTests.java
+++ tests/GuiTests.java
@@ -1,5 +1,10 @@
 
+import game.Gameboard;
+import game.Point;
 import game.ui.MainWindow;
+import java.awt.event.KeyEvent;
+import static java.awt.event.KeyEvent.CHAR_UNDEFINED;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GuiTests {
@@ -8,5 +13,21 @@ public class GuiTests {
     public void testSwingWindowCreation() {
         MainWindow ui = new MainWindow();
         assert ui.getComponentCount() > 0;
+    }
+
+    @Test
+    public void testSwingKeyPressesMovesCharacter() {
+        // Arrange
+        Gameboard board = new Gameboard(20, 20);
+        MainWindow ui = new MainWindow(board);
+
+        // Arrange
+        Point charPos = board.getCharPosition();
+        KeyEvent keyEvt = new KeyEvent(ui, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, CHAR_UNDEFINED);
+        ui.dispatchEvent(keyEvt);
+        Point newCharPos = board.getCharPosition();
+
+        // Assert
+        assertNotSame(charPos, newCharPos);
     }
 }
