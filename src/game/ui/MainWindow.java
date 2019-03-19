50c3ee3f4bf60472f6820b21f621f03058602278
Emil <38663830+spirotris@users.noreply.github.com>
2019-03-19 16:16:57 +0100

    Refactored image draws to its own method

--- src/game/ui/MainWindow.java
+++ src/game/ui/MainWindow.java
@@ -7,7 +7,6 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import javax.swing.Timer;
 
@@ -17,22 +16,10 @@ public class MainWindow extends javax.swing.JFrame {
     private final int DELAY = 50;
 
     public MainWindow() {
-        this(new Gameboard(20, 20));
-    }
-    
-    public MainWindow(Gameboard board) {
-        this.board = board;
+        this.board = new Gameboard();
         initComponents();
-        board.getPoint(12, 16).setStatus(Gameboard.MONSTER);
-        board.getPoint(8, 10).setStatus(Gameboard.TREASURE);
-        board.getPoint(3, 2).setStatus(Gameboard.WALL);
-        board.getPoint(3, 3).setStatus(Gameboard.WALL);
-        board.getPoint(3, 4).setStatus(Gameboard.WALL);
-        Timer timer = new Timer(DELAY, new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                repaint();
-            }
+        Timer timer = new Timer(DELAY, (final ActionEvent e) -> {
+            repaint();
         });
         timer.start();
     }
@@ -46,9 +33,7 @@ public class MainWindow extends javax.swing.JFrame {
         setBounds(new java.awt.Rectangle(0, 0, 0, 0));
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setMaximumSize(null);
-        setMinimumSize(new java.awt.Dimension(325, 348));
-        setPreferredSize(null);
-        setResizable(false);
+        setMinimumSize(new java.awt.Dimension(660, 660));
         addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 formKeyPressed(evt);
@@ -88,44 +73,56 @@ public class MainWindow extends javax.swing.JFrame {
     // End of variables declaration//GEN-END:variables
     @Override
     public void paint(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-
-        Image floor = Toolkit.getDefaultToolkit().getImage("src/res/graphics/floor_1.png");
-        Image wall = Toolkit.getDefaultToolkit().getImage("src/res/graphics/wall_right.png");
-        Image character = Toolkit.getDefaultToolkit().getImage("src/res/graphics/imp_idle_anim_f0.png");
-        Image ladder = Toolkit.getDefaultToolkit().getImage("src/res/graphics/floor_ladder.png");
-        Image treasure = Toolkit.getDefaultToolkit().getImage("src/res/graphics/chest_empty_open_anim_f0.png");
-        Image monster = Toolkit.getDefaultToolkit().getImage("src/res/graphics/goblin_idle_anim_f0.png");
-
         for (int i = 0; i < board.getWidth(); i++) {
             for (int j = 0; j < board.getHeight(); j++) {
-                int x = j * 16 + 25;
-                int y = i * 16+3;
-                switch (board.getPoint(j, i).getStatus()) {
-                    case Gameboard.WALL:
-                        g2.drawImage(wall, y, x, this);
-                        break;
-                    case Gameboard.FLOOR:
-                        g2.drawImage(floor, y, x, this);
-                        break;
-                    case Gameboard.CHARACTER:
-                        g2.drawImage(floor, y, x, this);
-                        g2.drawImage(character, y, x, this);
-                        break;
-                    case Gameboard.DOOR:
-                        g2.drawImage(ladder, y, x, this);
-                        break;
-                    case Gameboard.TREASURE:
-                        g2.drawImage(treasure, y, x, this);
-                        break;
-                    case Gameboard.MONSTER:
-                        g2.drawImage(floor, y, x, this);
-                        g2.drawImage(monster, y, x, this);
-                        break;
-                    default:
-                        g2.drawImage(floor, y, x, this);
-                }
+                drawTile(board.getPoint(j, i).getStatus(), g, j, i);
             }
+        }
+    }
+
+    public void drawTile(int type, Graphics g, int x, int y) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        x = x * 32 + 25;
+        y = y * 32 + 10;
+        int tileSize = 32;
+
+        Image floor = Toolkit.getDefaultToolkit()
+                .getImage("src/res/graphics/floor_1.png");
+        Image wall = Toolkit.getDefaultToolkit()
+                .getImage("src/res/graphics/wall_right.png");
+        Image character = Toolkit.getDefaultToolkit()
+                .getImage("src/res/graphics/imp_idle_anim_f0.png");
+        Image ladder = Toolkit.getDefaultToolkit()
+                .getImage("src/res/graphics/floor_ladder.png");
+        Image treasure = Toolkit.getDefaultToolkit()
+                .getImage("src/res/graphics/chest_empty_open_anim_f0.png");
+        Image monster = Toolkit.getDefaultToolkit()
+                .getImage("src/res/graphics/goblin_idle_anim_f0.png");
+
+        switch (type) {
+            case Gameboard.WALL:
+                g2.drawImage(wall, y, x, tileSize, tileSize, this);
+                break;
+            case Gameboard.FLOOR:
+                g2.drawImage(floor, y, x, tileSize, tileSize, this);
+                break;
+            case Gameboard.CHARACTER:
+                g2.drawImage(floor, y, x, tileSize, tileSize, this);
+                g2.drawImage(character, y, x, tileSize, tileSize, this);
+                break;
+            case Gameboard.DOOR:
+                g2.drawImage(ladder, y, x, tileSize, tileSize, this);
+                break;
+            case Gameboard.TREASURE:
+                g2.drawImage(treasure, y, x, tileSize, tileSize, this);
+                break;
+            case Gameboard.MONSTER:
+                g2.drawImage(floor, y, x, tileSize, tileSize, this);
+                g2.drawImage(monster, y, x, tileSize, tileSize, this);
+                break;
+            default:
+                g2.drawImage(floor, y, x, this);
         }
     }
 }
