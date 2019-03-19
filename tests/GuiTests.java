7a4eb0bb29b19243110c7d1639887a3eb3420ec8
Emil <38663830+spirotris@users.noreply.github.com>
2019-03-19 20:33:41 +0100

    Update GuiTests.java

--- tests/GuiTests.java
+++ tests/GuiTests.java
@@ -6,35 +6,42 @@ import java.awt.AWTEvent;
 import java.awt.event.KeyEvent;
 import static java.awt.event.KeyEvent.CHAR_UNDEFINED;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GuiTests {
 
+    MainWindow ui = null;
+    
+    @Before
+    public void setUp() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            ui = new MainWindow();
+        });
+    }
+    
     @Test
     public void testSwingWindowCreation() {
-        MainWindow ui = new MainWindow();
         assert ui.getComponentCount() > 0;
     }
 
     @Test
     public void testSwingRightKeyMovesCharacterRight() {
-        // Arrange
-        MainWindow ui = new MainWindow();
-        Gameboard board = ui.getBoard();
-
-        // Arrange
-        Point charPos = board.getCharacterPosition();
-        System.out.println(charPos.getX());
-        KeyEvent keyEvt = new KeyEvent(ui, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, CHAR_UNDEFINED);
-        ui.getComponent(0).dispatchEvent(keyEvt);
-        
-        Point newCharPos = board.getCharacterPosition();
-        System.out.println(newCharPos.getX());
-
-        // Assert
-        assertNotSame(charPos, newCharPos);
+        try {
+            SwingUtilities.invokeAndWait(() -> {
+                Gameboard board = ui.getBoard();
+                Point charPos = board.getCharacterPosition();
+                ui.dispatchEvent(new KeyEvent(ui, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, CHAR_UNDEFINED));
+                Point newCharPos = board.getCharacterPosition();
+                assertNotSame(charPos, newCharPos);
+            });
+        } catch (InterruptedException | InvocationTargetException ex) {
+            fail(ex.getLocalizedMessage());
+        }
     }
 }
