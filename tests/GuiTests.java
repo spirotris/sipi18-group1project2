900e7821a0073f38c6556bed9fcf1b42453ebeb4
Emil <38663830+spirotris@users.noreply.github.com>
2019-03-19 20:12:58 +0100

    Fixed GUI tests after Gameboard refactor

--- tests/GuiTests.java
+++ tests/GuiTests.java
@@ -2,8 +2,12 @@
 import game.Gameboard;
 import game.Point;
 import game.ui.MainWindow;
+import java.awt.AWTEvent;
 import java.awt.event.KeyEvent;
 import static java.awt.event.KeyEvent.CHAR_UNDEFINED;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -16,16 +20,19 @@ public class GuiTests {
     }
 
     @Test
-    public void testSwingKeyPressesMovesCharacter() {
+    public void testSwingRightKeyMovesCharacterRight() {
         // Arrange
-        Gameboard board = new Gameboard(20, 20);
-        MainWindow ui = new MainWindow(board);
+        MainWindow ui = new MainWindow();
+        Gameboard board = ui.getBoard();
 
         // Arrange
-        Point charPos = board.getCharPosition();
+        Point charPos = board.getCharacterPosition();
+        System.out.println(charPos.getX());
         KeyEvent keyEvt = new KeyEvent(ui, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, CHAR_UNDEFINED);
-        ui.dispatchEvent(keyEvt);
-        Point newCharPos = board.getCharPosition();
+        ui.getComponent(0).dispatchEvent(keyEvt);
+        
+        Point newCharPos = board.getCharacterPosition();
+        System.out.println(newCharPos.getX());
 
         // Assert
         assertNotSame(charPos, newCharPos);
