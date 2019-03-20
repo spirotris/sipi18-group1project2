a1c82763093ed3a249ad6f5e2dc031d7a308e5cf
Marcus Laitala <marcus.laitala@gmail.com>
2019-03-20 09:23:58 +0100

    Added tests for Player collision with treasures, Door and lasers

--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -1,7 +1,7 @@
 /**
  * @author Marcus Laitala
- * @Date 2019-03-17 * 
- * @version 0.1
+ * @Date 2019-03-17 
+ * @version 0.3
  */
 
 import junitparams.JUnitParamsRunner;
@@ -61,21 +61,74 @@ public class PlayerTest {
 		Point po, treasure;
 		
 		//Act
-		//Sätt spelaren på plats 0,0
+		//Move the player onto point 0,0
 		po = g.getPoint(0,0);
-		po.setStatus(2); //2 = Player
+		po.setStatus(TileType.CHARACTER);
 		
-		//Skapa en skatt på plats 0,1
+		//Create a treasure at 0,1
 		treasure = g.getPoint(0,1);
 		treasure.setStatus(TileType.TREASURE);
 		int actual = 0;
-		//Flytta spelaren till skatten
+		//Move player onto treasure
 		if(g.moveCharacter(Direction.DOWN) == TileType.TREASURE) {
 			p.setTreasure(nrOfFoundTreasures);
 			actual = p.getTreasure();
 		}
-		
+			
 		//Assert
 		assertEquals(actual, nrOfFoundTreasures);
+	}
+		
+	@Test
+	Parameters({"true", "false"})	
+	public void testPlayerWalkingOntoDoorWithTreasures(boolean hasTreasure ) {
+		//Arrange		
+		Gameboard g = new Gameboard();
+		Point po, door;
+			
+		//Move the player onto point 0,0
+		po = g.getPoint(0,0);
+		po.setStatus(TileType.CHARACTER);
+		
+		//Create a treasure at 0,1
+		door = g.getPoint(0,1);
+		door.setStatus(TileType.DOOR);
+			
+		//Act
+		boolean actual = false;
+		//Move player onto door
+		if(g.moveCharacter(Direction.DOWN) == TileType.DOOR) {
+			actual = true;
 		}
+		
+		//Assert
+		if(hasTreasure)
+			assertTrue(actual);
+		else
+			assertFalse(actual);
+	}
+	
+	@Test
+	public void testPlayerWalkingOntoLaseResultsInGameOver() {
+		//Arrange		
+		Gameboard g = new Gameboard();
+		Point po, laser;
+		
+		g.setPlayerAlive(True);
+		
+		//Move the player onto point 0,0
+		po = g.getPoint(0,0);
+		po.setStatus(TileType.CHARACTER);
+		
+		//Create a laser at 0,1
+		door = g.getPoint(0,1);
+		door.setStatus(TileType.LASER);
+		
+		//Act
+		if(g.moveCharacter(Direction.Down) == TileType.LASER)
+				g.setPlayerAlive(false);
+		
+		//Assert
+		assertFalse(g.getPlayerAlive());
+	}
 }
