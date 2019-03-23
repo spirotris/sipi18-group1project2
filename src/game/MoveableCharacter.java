ba75de0aadc81f1a3a5a84e985402c9eb028f38b
Marcizz <46536014+Marcizz@users.noreply.github.com>
2019-03-23 20:51:32 +0100

    Create MoveableCharacter.java
    Refactor for Player and Monsters

---
+++ src/game/MoveableCharacter.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package game;
+
+/**
+ * @author Marcus Laitala
+ * @Date 2019-03-23
+ */
+public abstract class MoveableCharacter extends Point {
+
+	/**
+	 * @param y
+	 * @param x
+	 * @param tileType
+	 */
+	public MoveableCharacter(int y, int x, TileType tileType) {
+		super(y, x, tileType);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
