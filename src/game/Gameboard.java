1b4c864b0c4c0f3ba212d3e53883e398879f04a7
spirotris <38663830+spirotris@users.noreply.github.com>
2019-03-24 10:28:15 +0100

    Refactored moveCharacter

--- src/game/Gameboard.java
+++ src/game/Gameboard.java
@@ -40,51 +40,36 @@ public class Gameboard {
 
     // Moving character in desired direction
     public boolean moveCharacter(Direction direction) {
-        int y = player.getY();
-        int x = player.getX();
+        int newY = player.getY(), oldY = newY;
+        int newX = player.getX(), oldX = newX;
         int move = direction.getValue();
-
+        
         if (direction.equals(Direction.RIGHT) || direction.equals(Direction.LEFT)) {
-            if (!onCollision(boardGrid[y][x + move])) {
-                // Mostly, the player will be walking on floor, makes sense to have this first
-                if (!isPlayerOnDoor) {
-                    // Move the character on the board
-                    fixPreviousTile(y, x, FLOOR);
-                    fixPreviousTile(y, x + move, CHARACTER);
-                } else {
-                    if (boardGrid[y][x + move].getTileType() == DOOR) {
-                        fixPreviousTile(y, x, FLOOR);
-                    } else {
-                        fixPreviousTile(y, x, DOOR);
-                    }
-                    // Move the character on the board
-                    fixPreviousTile(y, x + move, CHARACTER);
-                }
-                // Update the players coordinates
-                player.move(boardGrid[y][x + move]);
-                return true;
-            }
-        } else if (direction.equals(Direction.UP) || direction.equals(Direction.DOWN)) {
-            if (!onCollision(boardGrid[y + move][x])) {
-                // Mostly, the player will be walking on floor, makes sense to have this first
-                if (!isPlayerOnDoor) {
-                    // Move the character on the board
-                    fixPreviousTile(y, x, FLOOR);
-                    fixPreviousTile(y + move, x, CHARACTER);
-                } else {
-                    if (boardGrid[y + move][x].getTileType() == DOOR) {
-                        fixPreviousTile(y, x, FLOOR);
-                    } else {
-                        fixPreviousTile(y, x, DOOR);
-                    }
-                    // Move the character on the board
-                    fixPreviousTile(y + move, x, CHARACTER);
-                }
-                // Update the players coordinates
-                player.move(boardGrid[y + move][x]);
+            newX = oldX + move;
+        }
+        if (direction.equals(Direction.UP) || direction.equals(Direction.DOWN)) {
+            newY = oldY + move;
+        }
 
-                return true;
+        if (!onCollision(boardGrid[newY][newX])) {
+            // Mostly, the player will be walking on floor, makes sense to have this first
+            if (!isPlayerOnDoor) {
+                // Move the character on the board
+                fixPreviousTile(oldY, oldX, FLOOR);
+                fixPreviousTile(newY, newX, CHARACTER);
+            } else {
+                if (boardGrid[newY][newX].getTileType() == DOOR) {
+                    fixPreviousTile(oldY, oldX, FLOOR);
+                } else {
+                    fixPreviousTile(oldY, oldX, DOOR);
+                }
+                // Move the character on the board
+                fixPreviousTile(newY, newX, CHARACTER);
+
             }
+            // Update the players coordinates
+            player.move(boardGrid[newY][newX]);
+            return true;
         }
 
         // There was a wall in the way
